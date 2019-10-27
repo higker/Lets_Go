975240aae1847fef1445449349f819bb551ff1b9
YooDing <2420498526@qq.com>
2019-10-27 16:42:33 +0800

    updated fmt example

---
+++ lets_04_fmt/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//fmt占位符
+func main() {
+	var n int = 100
+	fmt.Printf("查看类型%T\n", n)
+	fmt.Printf("查看值%v\n", n)
+	fmt.Printf("二进制%b \n", n)
+	fmt.Printf("十进制%d\n", n)
+	fmt.Printf("八进制%o\n", n)
+	fmt.Printf("十六进制%x\n", n)
+	fmt.Printf("字符串%s\n", "字符串")
+}
