8e5e41e6dbfb91fc5674a80562f1a71fd35c0742
YooDing <2420498526@qq.com>
2019-10-27 17:19:59 +0800

    updated new source code 😝

--- lets_04_string/main.go
+++ lets_04_string/main.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
-	s = "string"
-	c = 'S'
-	t = 'T'
-	r = 'R'
+	s      = "string"
+	c      = 'S'
+	t      = 'T'
+	r      = 'R'
+	string = `
+		My name is "Ding Shuo",
+		My age is 20
+		Useing langues Chinese
+  	`
 )
 
 func main() {
@@ -14,4 +22,31 @@ func main() {
 	fmt.Println(c)
 	fmt.Println(t)
 	fmt.Println(r)
+	fmt.Println(string)
+
+	//字符串相关操作
+	fmt.Println("字符串长度", len(string))
+
+	//字符串拼接
+	name := "Go"
+	age := "20"
+	ss := name + age
+	fmt.Println(ss)
+	//字符串格式化拼接 并且 格式化
+	ss1 := fmt.Sprintf("我的名字叫%s 年龄%s", name, age)
+	fmt.Println(ss1)
+
+	url := "www.google.com"
+	result := strings.Split(url, ".")
+	fmt.Println(result)
+	//是否包含某个字符串
+	var isContains bool = strings.Contains(url, "google")
+	fmt.Println(isContains)
+	//是否以什么开头 前缀
+	fmt.Println(strings.HasPrefix(url, "www"))
+	//是否以什么结尾 后缀
+	fmt.Println(strings.HasSuffix(url, "com"))
+	//判断子串出现的位置
+	fmt.Println(strings.Index(url, "google"))
+
 }
